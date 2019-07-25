0c18454bba1efd6ce2673670ef95415a390ab4a7
Hunter Nakagawa <hunternakagawa@gmail.com>
2019-07-24 20:13:19 -1000

    changed api endpoint to reflect new api (bsmapi-php)

--- BSM/CLASSES/CLASS_SERVER_API.cs
+++ BSM/CLASSES/CLASS_SERVER_API.cs
@@ -12,7 +12,7 @@ namespace Borealis
         {
             using (var webClient = new System.Net.WebClient())
             {
-                var json = webClient.DownloadString("http://sfo2.hauteclaire.me:1337/config/" + appID);
+                var json = webClient.DownloadString("https://bsm.hauteclaire.me/" + appID);
                 return JsonConvert.DeserializeObject<GameServerObject>(json);
             }
         }
@@ -24,7 +24,7 @@ namespace Borealis
         {
             using (var webClient = new System.Net.WebClient())
             {
-                var json = webClient.DownloadString("http://sfo2.hauteclaire.me:1337/index");
+                var json = webClient.DownloadString("https://bsm.hauteclaire.me");
                 JObject o = JObject.Parse(json);
 
                 foreach (var serverAppIDLink in o)
